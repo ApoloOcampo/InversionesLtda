d79a181e72d94d2a18b3b2499a90c0db74531759
Apolo Ocampo <apolo.ocamporios@gmail.com>
2024-04-21 17:06:59 -0400

    Creando nueva clase "Producto" y su controlador.

---
+++ InversionesLtda.API/Controllers/ProductoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using InversionesLtda.API.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace InversionesLtda.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductoController : ControllerBase
+    {
+        private static IList<Producto> lista = new List<Producto>();
+
+        // GET: api/<ProductoController>
+        [HttpGet]
+        public IEnumerable<Producto> Get()
+        {
+            return lista;
+        }
+
+        // GET api/<ProductoController>/5
+        [HttpGet("{id}")]
+        public Producto Get(int id)
+        {
+            return lista.FirstOrDefault(x => x.id == id);
+        }
+
+        // POST api/<ProductoController>
+        [HttpPost]
+        public void Post([FromBody] Producto value)
+        {
+            lista.Add(value);
+        }
+
+        // PUT api/<ProductoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Producto value)
+        {
+            Producto selecion = lista.FirstOrDefault(x => x.id == id);
+            lista[lista.IndexOf(selecion)] = value;
+        }
+
+        // DELETE api/<ProductoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            lista.Remove(lista.FirstOrDefault(x => x.id == id));
+        }
+    }
+}
---
+++ InversionesLtda.API/Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace InversionesLtda.API.Models
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int precio { get; set; }
+    }
+}
